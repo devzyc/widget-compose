46c616fdc0fb43403eab93ea7917a1b7118968ca
zengyongchang <zeng_yong_chang@163.com>
2022-04-01 09:22:05 +0800

    little

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         with(Dep.Gradle) {
             classpath(android)
             classpath(kotlinPlugin)
